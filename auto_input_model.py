6e790d00a0e246a4fe499a998961a10c40b82912
andrew-impell <andrew-impell@users.noreply.github.com>
2020-06-11 22:16:36 -0400

    Update auto_input_model.py

--- auto_input_model.py
+++ auto_input_model.py
@@ -4,11 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as pdr
 from matplotlib import style
-import os
 from decouple import config
+import matplotlib
 
+# matplotlib.rcParams['text.usetex'] = True
 style.use('ggplot')
-
 quandl_key = config('Q_KEY')
 
 
@@ -138,12 +138,87 @@ autos.index = pd.to_datetime(autos.index)
 synthetic = synthetic.iloc[-1300:-1, 1]
 
 
-for auto in list(autos.columns):
+def get_correlation_dict(lag=1):
 
-    syn_auto_df = pd.concat([autos[auto], synthetic], axis=1)
-    syn_auto_df.dropna(axis=0, how='any', inplace=True)
-    syn_auto_df.interpolate(method='from_derivatives', inplace=True)
+    cor_dict = {}
 
-    cor = syn_auto_df.corr().values[0, 1]
+    for auto in list(autos.columns):
 
-    print(f'{auto} correlation: {cor:.2f}')
+        syn_auto_df = pd.concat([autos[auto],
+                                 synthetic.shift(lag, fill_value=0)], axis=1)
+        syn_auto_df.dropna(axis=0, how='any', inplace=True)
+        syn_auto_df.interpolate(method='from_derivatives', inplace=True)
+
+        cor = syn_auto_df.corr().values[0, 1]
+        cor_dict[auto] = cor
+
+        print(f'{auto} correlation: {cor:.2f}')
+
+    sorted_dict = \
+        {k: v for k, v in sorted(cor_dict.items(), key=lambda item: item[1])}
+
+    return sorted_dict, cor_dict
+
+
+sorted_dict, cor_dict = get_correlation_dict(lag=0)
+
+# auto_mean = synthetic.mean() / autos.mean().mean()
+
+
+def opt_lag_val(max_lag=100):
+    max_ss = 0
+    best_i = 0
+    ss_set = []
+    for i in range(-max_lag, max_lag):
+        sorted_dict, cor_dict = get_correlation_dict(lag=i)
+        temp_ss = np.mean(np.abs(np.array(list(sorted_dict.values()))))
+        ss_set.append(temp_ss)
+        if temp_ss > max_ss:
+            max_ss = temp_ss
+            best_i = i
+    return best_i, ss_set
+
+
+best_i, ss_set = opt_lag_val()
+print('Best lag: ', best_i)
+
+max_lag = 100
+plt.xlabel('Lag')
+plt.ylabel('mean(abs(correlations))')
+plt.title('Optimal Lag')
+plt.plot(np.arange(-max_lag, max_lag), ss_set)
+plt.show()
+
+
+def graph_correlations(sorted_dict):
+    plt.figure()
+    plt.subplots_adjust(hspace=.5)
+
+    plt.subplot(211)
+    plt.title('Negative Correlation')
+    plt.xlabel('Date')
+    plt.ylabel('Percent Change')
+    plt.plot(synthetic.index, synthetic.pct_change().values,
+             label='Synthetic', alpha=0.5)
+    for comp in list(sorted_dict.keys())[:3]:
+        cor_label = str(comp) + ": " + str(cor_dict[comp])
+        plt.plot(autos.index, autos[comp].pct_change().values,
+                 label=cor_label, alpha=0.5)
+    plt.legend()
+
+    plt.subplot(212)
+    plt.title('Positive Correlation')
+    plt.xlabel('Date')
+    plt.ylabel('Percent Change')
+    plt.plot(synthetic.index, 50 * synthetic.pct_change().values,
+             label='Synthetic', alpha=0.5)
+    for comp in list(sorted_dict.keys())[-3:]:
+        cor_label = str(comp) + ": " + str(cor_dict[comp])
+        plt.plot(autos.index, autos[comp].pct_change().values,
+                 label=cor_label, alpha=0.5)
+    plt.legend()
+    plt.show()
+    return 0
+
+
+a = graph_correlations(sorted_dict)
